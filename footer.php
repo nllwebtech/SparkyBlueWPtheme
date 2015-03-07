2bc3df44a4d7c6117b92e7289ff9e32d59675955
Nicole L.L. <nllwebtech@users.noreply.github.com>
2015-03-07 01:26:59 -0500

    footer.php

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer>
+	
+		 
+	<nav id="footnav"><?php
+
+$defaults = array(
+	'theme_location'  => '',
+	'menu'            => '',
+	'container'       => 'div',
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => 'menu',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	'depth'           => 0,
+	'walker'          => ''
+);
+
+wp_nav_menu( $defaults );
+
+?></nav>
+	
+	<small>Copyright 2015 - <a href="http://www.hirenicolell.info">Nicole L.L.</a></small>
+</footer>
